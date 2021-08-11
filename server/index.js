99bc61228c4694602004accd538f66d818bd8f60
Muhammad Ali khan <57296152+alykhan2212@users.noreply.github.com>
2021-08-11 16:50:56 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
